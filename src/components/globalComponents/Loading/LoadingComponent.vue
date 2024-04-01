e59cc4c9f28b593cb4e166c7a46d9b04e608be42
tms-5 <tms5@cin.ufpe.br>
2024-04-01 16:59:46 -0300

    Add LoadingComponent.vue and style.scss

---
+++ src/components/globalComponents/Loading/LoadingComponent.vue
@@ -0,0 +1,30 @@
+<script>
+import './style.scss'
+export default {
+  props: {
+    size: {
+      type: String,
+      default: 'medium',
+      validator: (value) => ['big', 'medium', 'small'].includes(value)
+    }
+  }
+}
+</script>
+<template>
+  <div class="loading">
+    <div :class="['loading-spinner', size]" role="spinbutton">
+      <div class="bar1"></div>
+      <div class="bar2"></div>
+      <div class="bar3"></div>
+      <div class="bar4"></div>
+      <div class="bar5"></div>
+      <div class="bar6"></div>
+      <div class="bar7"></div>
+      <div class="bar8"></div>
+      <div class="bar9"></div>
+      <div class="bar10"></div>
+      <div class="bar11"></div>
+      <div class="bar12"></div>
+    </div>
+  </div>
+</template>
